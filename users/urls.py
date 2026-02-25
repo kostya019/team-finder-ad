0a6fc8d094a016fb6efe9a78b1e7b3b20e99e07e
Konstantin Khelin <kostya.xelin.07@mail.ru>
2026-02-26 04:32:42 +0500

    create logout, profile, edit_profile pages

--- users/urls.py
+++ users/urls.py
@@ -8,8 +8,9 @@ app_name = 'users'
 urlpatterns = [
     path('<int:user_id>/', views.profile, name='profile'),
     path('list/', views.user_list, name='user_list'),
-    path('edit-profile/', views.edit_ptofile, name='edit_profile'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
     path('change_password/', auth_views.PasswordChangeView.as_view(template_name='users/change_password.html', success_url='change_password/'), name='change_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
+from projects.models import Project
 from .models import User
+from .forms import UserForm
+
 
 def profile(request, user_id):
-    pass
+    profile = get_object_or_404(User, id=user_id)
+    context = {'user': profile}
+    return render(request, 'users/user-details.html', context)
 
 def user_list(request):
     template = 'users/participants.html'
@@ -23,5 +31,24 @@ def user_list(request):
 def register(request):
     pass
 
-def edit_ptofile(request):
-    pass
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST or None, instance=request.user)
+
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.save()
+            return redirect('/projects/list/')
+
+    form = UserForm(instance=request.user)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'users/edit_profile.html', context)
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('/projects/list/')
